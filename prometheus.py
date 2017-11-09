3adc57de08741f092fa90009756dc0ed41540bd6
antb59 <antb59@gmail.com>
2017-11-09 21:34:03 +0000

    upload

--- prometheus.py
+++ prometheus.py
@@ -3,12 +3,18 @@
 
 import time
 from flask import Flask, jsonify, render_template, request, redirect
-
+from werkzeug import secure_filename
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    return render_template('index.html')
+
+@app.route('/upload', methods = ['POST'])
+def upload():
+   f = request.files['file']
+   f.save(secure_filename(f.filename))
+   return 'file uploaded successfully'
 
 @app.route('/toggle',methods=['POST'])
 def toggle():
